e35859d445f06378e7dc5ce470e0020b79edc097
Kacper Urban <urbankacper613@gmail.com>
2026-07-11 01:05:03 +0200

    Pretrain LLM on small dataset

--- scripts/pretraining.py
+++ scripts/pretraining.py
@@ -1,3 +1,8 @@
+from scripts.text_generation import (
+    generate_simple_text,
+    text_to_tokens_ids,
+    tokens_ids_to_text,
+)
 import tiktoken
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -67,3 +72,68 @@ def calc_loss_dataloader(dataloader, model, device, num_batches=None):
         else:
             break
     return total_loss / num_batches
+
+
+def train_llm(
+    model,
+    train_dataloader,
+    val_dataloader,
+    optimizer,
+    num_epoch,
+    device,
+    eval_freq,
+    eval_iter,
+    start_context,
+    tokenizer,
+):
+    track_train_loss, track_val_loss, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for i in range(num_epoch):
+        model.train()
+        for input, target in train_dataloader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input, target, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = eval_model(
+                    train_dataloader, val_dataloader, model, device, eval_iter
+                )
+                track_train_loss.append(train_loss)
+                track_val_loss.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Epoch: {i+1}, step: {global_step:06d}")
+                print(f"Train loss: {train_loss:.3f}, Valid loss: {val_loss:.3f}")
+                print(
+                    f"Training perplexity: {torch.exp(torch.tensor(train_loss)):.3f}, Valid perplexity: {torch.exp(torch.tensor(val_loss)):.3f}"
+                )
+
+        generate_and_print_sample(model, tokenizer, start_context, device)
+    return track_train_loss, track_val_loss, track_tokens_seen
+
+
+def eval_model(train_dataloader, val_dataloader, model, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_dataloader(
+            train_dataloader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_dataloader(
+            val_dataloader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+
+def generate_and_print_sample(model, tokenizer, start_context, device):
+    model.eval()
+    context_length = model.pos_emb.weight.shape[0]
+    input_tokens = text_to_tokens_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        output = generate_simple_text(model, input_tokens, 50, context_length)
+    decoded_output = tokens_ids_to_text(output, tokenizer)
+    print(decoded_output.replace("\n", " "))
+    model.train()
